e8778d344dc4cde98a3e61e10818a0d020cac194
Michel Marion <kangkm19@gmail.com>
2023-10-14 11:09:43 -0400

    Fix register3

--- app.js
+++ app.js
@@ -17,8 +17,9 @@ const reviewRoutes = require('./routes/reviews');
 const LocalStrategy = require('passport-local');
 const User = require('./models/user');
 const helmet = require("helmet");
-const dbUrl = process.env.DB_URL
-// mongoose.connect('mongodb://localhost:27017/yelp-camp')
+const dbUrl = process.env.DB_URL || 'mongodb://localhost:27017/yelp-camp'
+
+mongoose.connect(dbUrl);
 
 const db = mongoose.connection;
 db.on("error", console.error.bind(console, "connection error:"));
