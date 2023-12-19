01b99c009947a21b76addfb8a96bd09583e40935
Michel Marion <kangkm19@gmail.com>
2023-12-18 21:56:59 -0500

    Update Content Security Policy

--- app.js
+++ app.js
@@ -75,6 +75,7 @@ const scriptSrcUrls = [
   "https://cdnjs.cloudflare.com/",
   "https://cdn.jsdelivr.net",
   "https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css",
+  "https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/css/bootstrap.min.css",
 ];
 const styleSrcUrls = [
   "https://kit-free.fontawesome.com/",
@@ -84,6 +85,7 @@ const styleSrcUrls = [
   "https://fonts.googleapis.com/",
   "https://use.fontawesome.com/",
   "https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css",
+  "https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/css/bootstrap.min.css",
 ];
 const connectSrcUrls = [
   "https://api.mapbox.com/",
@@ -91,6 +93,7 @@ const connectSrcUrls = [
   "https://b.tiles.mapbox.com/",
   "https://events.mapbox.com/",
   "https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css",
+  "https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/css/bootstrap.min.css",
 ];
 const fontSrcUrls = [];
 app.use(
